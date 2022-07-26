e00a74a0c0cdc8465a00988f6de8f96d3bda1c94
Ricardo <ricardoalbuquerquecorretor@gmail.com>
2022-07-26 10:32:14 -0300

    subir

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -2,6 +2,8 @@ package br.com.alura.linguagens.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -16,6 +18,13 @@ public class LinguagemController {
        List<Linguagem> linguagens =  repositorio.findAll();
        return linguagens;
    }
+   @PostMapping ("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+         Linguagem linguagemSalva = repositorio.save(linguagem);
+         return linguagemSalva;
+   }
+
+
 }
 
 
